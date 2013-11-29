7e55d9e17d901cd7b745f0774a94a122bfbe7db2
Sebastian Iacomuzzi <sebastian.iacomuzzi@gmail.com>
2013-11-29 16:33:31 -0300

    first commit

---
+++ AdLdap.SettingsDetector/AdLdap.SettingsDetector/Extensions.cs
@@ -0,0 +1,66 @@
+﻿namespace AdLdap.SettingsDetector
+{
+    using System;
+    using System.Text;
+
+    internal static class Extensions
+    {
+        public static string CleanForJSON(this string originalString)
+        {
+            if (string.IsNullOrEmpty(originalString))
+            {
+                return string.Empty;
+            }
+
+            char c = '\0';
+            var sb = new StringBuilder(originalString.Length + 4);
+            string t;
+
+            for (var i = 0; i < originalString.Length; i += 1)
+            {
+                c = originalString[i];
+
+                switch (c)
+                {
+                    case '\\':
+                    case '"':
+                        sb.Append('\\');
+                        sb.Append(c);
+                        break;
+                    case '/':
+                        sb.Append('\\');
+                        sb.Append(c);
+                        break;
+                    case '\b':
+                        sb.Append("\\b");
+                        break;
+                    case '\t':
+                        sb.Append("\\t");
+                        break;
+                    case '\n':
+                        sb.Append("\\n");
+                        break;
+                    case '\f':
+                        sb.Append("\\f");
+                        break;
+                    case '\r':
+                        sb.Append("\\r");
+                        break;
+                    default:
+                        if (c < ' ')
+                        {
+                            t = "000" + string.Format("X", c);
+                            sb.Append("\\u" + t.Substring(t.Length - 4));
+                        }
+                        else
+                        {
+                            sb.Append(c);
+                        }
+                        break;
+                }
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ AdLdap.SettingsDetector/AdLdap.SettingsDetector/Program.cs
@@ -0,0 +1,83 @@
+﻿namespace AdLdap.SettingsDetector
+{
+    using System;
+    using System.Collections.Generic;
+    using System.DirectoryServices;
+    using System.DirectoryServices.AccountManagement;
+    using System.DirectoryServices.ActiveDirectory;
+    using System.Linq;
+    using System.Security.Principal;
+
+    class Program
+    {
+        private static string resultTemplate = "{{ " +
+            "\"domainDNS\": \"{0}\", " +
+            "\"baseDN\": \"{1}\", " +
+            "\"domainController\": \"{2}\", " +
+            "\"userLoggedIn\": \"{3}\" " +
+            "\"windowsIdentity\": \"{4}\" " +
+        "}}";
+
+        private static string errorTemplate = "{{ " +
+            "\"error\": \"{0}\" " +
+        "}}";
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                var domainDNS = GetDomains().FirstOrDefault();
+                var baseDN = GetBaseDN(domainDNS);
+                var domainController = GetDomainController();
+                var userLoggedIn = Environment.UserName;
+                var windowsIdentity = WindowsIdentity.GetCurrent() != null ? WindowsIdentity.GetCurrent().Name : string.Empty;
+
+                // Print result
+                Console.WriteLine(
+                    resultTemplate,
+                    domainDNS.CleanForJSON(),
+                    baseDN.CleanForJSON(),
+                    domainController.CleanForJSON(),
+                    userLoggedIn.CleanForJSON(),
+                    windowsIdentity.CleanForJSON());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(errorTemplate, ex.Message.CleanForJSON());
+                Environment.Exit(1);
+            }
+        }
+
+        private static List<string> GetDomains()
+        {
+            var domains = new List<string>();
+
+            // Querying the current Forest for the domains within
+            foreach (Domain domain in Forest.GetCurrentForest().Domains)
+            {
+                domains.Add(domain.Name);
+            }
+
+            return domains;
+        }
+
+        private static string GetBaseDN(string domainDNS)
+        {
+            if (string.IsNullOrEmpty(domainDNS))
+            {
+                return string.Empty;
+            }
+
+            var rootDSE = new DirectoryEntry("LDAP://" + domainDNS);
+            return rootDSE.Properties["distinguishedName"].Value as string;
+        }
+
+        private static string GetDomainController()
+        {
+            using (var context = new PrincipalContext(ContextType.Domain))
+            {
+                return context.ConnectedServer;
+            }
+        }
+    }
+}
